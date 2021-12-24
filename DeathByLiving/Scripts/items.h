63d2592b96d4deea8dc20d30ff38085663e4de56
onesketchyguy <lowebros9812@gmail.com>
2021-12-23 23:51:15 -0800

    Update items.h
    Fixed a typo

--- DeathByLiving/Scripts/items.h
+++ DeathByLiving/Scripts/items.h
@@ -74,7 +74,7 @@ struct Item
 	}
 
 	// Read a JSON file
-	const static void LoadJsonData(std::vector<Item>& grades, std::string fileName = "Data/items.json")
+	const static void LoadJsonData(std::vector<Item>& items, std::string fileName = "Data/items.json")
 	{
 		std::ifstream fs;
 		fs.open(fileName);
@@ -103,7 +103,7 @@ struct Item
 				item.durValue = j.at("dur");
 				item.type = j.at("t");
 
-				grades.push_back(item);
+				items.push_back(item);
 			}
 
 			content.clear();
@@ -114,20 +114,23 @@ struct Item
 		std::cout << "Items loaded." << std::endl;
 	}
 
+	const static Item GetRandomWeapon();
+	const static Item GetRandomArmor();
+	const static Item GetRandomHealing();
 	const static Item GetRandomItem();
 };
 
 const Item Item::NULL_ITEM{ "NULLITEM", 0, 0, "NULL"};
 
-///  FIXME: This funciton is for test purposes ONLY!
-const Item Item::GetRandomItem()
+const Item Item::GetRandomWeapon()
 {
 	Item i;
-	
+	i.type = WEAPON_ITEM_TYPE;
+
 	int NAMES_LENGTH = 5;
 	std::string* names = new std::string[NAMES_LENGTH]
 	{
-		"stick", "sword", "rock", "lief", "thing"
+		"Stick", "Bronze sword", "Iron sword", "Steel sword", "Mythril sword"
 	};
 
 	const int QUALITY_LEVELS = 7;
@@ -138,75 +141,126 @@ const Item Item::GetRandomItem()
 
 	int quality = rand() % QUALITY_LEVELS;
 	int level = rand() % NAMES_LENGTH;
-	i.name = names[level];
 
-	i.keyValue = 0;
-	i.durValue = 0;
-
-	i.spriteCellX = 3;
+	i.spriteCellX = 0;
 	i.spriteCellY = 1 + (rand() % ITEM::MAX_CELL_Y);
+
+	i.name = qualityNames[quality] + names[level];
+	i.durValue = (level * 2) + rand() % (1 + quality * 3);
+	i.keyValue = level + rand() % (1 + quality * 2);
+
+
+	delete[] names;
+	delete[] qualityNames;
+
+	return i;
+}
+
+const Item Item::GetRandomArmor()
+{
+	Item i;
+	i.type = ARMOR_ITEM_TYPE;
+
+	int NAMES_LENGTH = 5;
+	std::string* names = new std::string[NAMES_LENGTH]
+	{
+		"Cloth", "Leather", "Iron", "Steel", "Mythril"
+	};
+
+	const int QUALITY_LEVELS = 7;
+	std::string* qualityNames = new std::string[QUALITY_LEVELS]
+	{
+		"Horrible ", "Worn ", "", "Nice ", "Great ", "Fantastic ", "Other worldly "
+	};
+
+	int quality = rand() % QUALITY_LEVELS;
+	int level = rand() % NAMES_LENGTH;
+	i.name = qualityNames[quality] + names[level] + " armor";
+	i.durValue = level + rand() % (1 + quality * 3);
+	i.keyValue = level + rand() % (1 + quality * 2);
+
+	i.spriteCellX = 2;
+	i.spriteCellY = 1 + (rand() % ITEM::MAX_CELL_Y);
+
+	delete[] names;
+	delete[] qualityNames;
+
+	return i;
+}
+
+const Item Item::GetRandomHealing()
+{
+	Item i;
+	i.type = HEALING_ITEM_TYPE;
+
+	int NAMES_LENGTH = 5;
+	std::string* names = new std::string[NAMES_LENGTH]
+	{
+		"red", "blue", "green", "purple","grey"
+	};
+
+	const int QUALITY_LEVELS = 7;
+	std::string* qualityNames = new std::string[QUALITY_LEVELS]
+	{
+		"Ugly ", "Curious ", "Smelly ", "Absurd ", "Fascinating ", "Curious ", "Enticing "
+	};
+
+	int quality = rand() % QUALITY_LEVELS;
+	int level = rand() % NAMES_LENGTH;
+	i.name = qualityNames[quality] + names[level] + " potion";
+	quality += 1;
+	i.durValue = 1 + rand() % 2;	
+	do i.keyValue = (quality - (rand() % (quality * 2))) * 2; while (i.keyValue == 0);
+
+	i.spriteCellX = 1;
+	i.spriteCellY = 1 + (rand() % ITEM::MAX_CELL_Y);
+
+	delete[] names;
+	delete[] qualityNames;
+
+	return i;
+}
+
+///  FIXME: This funciton is for test purposes ONLY!
+const Item Item::GetRandomItem()
+{
+	Item i;
+	
+	int NAMES_LENGTH = 5;
+	std::string* names = nullptr;
+
+	const int QUALITY_LEVELS = 7;
+	std::string* qualityNames = nullptr;
+
+	int quality = rand() % QUALITY_LEVELS;
+	int level = rand() % NAMES_LENGTH;
+
 
 	int t = rand() % ITEM_TYPES_LENGTH;
 
 	switch (t)
 	{
 	case 0:
-		i.type = ARMOR_ITEM_TYPE;
-		NAMES_LENGTH = 5;
-		names = new std::string[NAMES_LENGTH]
-		{
-			"Cloth", "Leather", "Iron", "Steel", "Mythril"
-		};
-
-		level = rand() % NAMES_LENGTH;
-		i.name = qualityNames[quality] + names[level] + " armor";
-		i.durValue = level + rand() % (1 + quality * 3);
-		i.keyValue = level + rand() % (1 + quality * 2);
-
-		i.spriteCellX = 2;		
-
+		return GetRandomArmor();
 		break;
 	case 1:
-		i.type = WEAPON_ITEM_TYPE;
-		NAMES_LENGTH = 6;
-		names = new std::string[NAMES_LENGTH]
-		{
-			"Rock", "Wooden sword", "Bronze sword", "Iron sword", "Steel sword", "Mythril sword"
-		};
-
-		level = rand() % NAMES_LENGTH;
-		i.name = qualityNames[quality] + names[level];
-		i.durValue = (level * 2) + rand() % (1 + quality * 3);
-		i.keyValue = level + rand() % (1 + quality * 2);
-
-		i.spriteCellX = 0;
-
+		return GetRandomWeapon();
 		break;
 	case 2:
-		i.type = HEALING_ITEM_TYPE;
-		NAMES_LENGTH = 5;
-		names = new std::string[NAMES_LENGTH]
-		{
-			"red", "blue", "green", "purple","grey"
-		};
-
-		qualityNames = new std::string[QUALITY_LEVELS]
-		{
-			"Ugly ", "Curious ", "Smelly ", "Absurd ", "Fascinating ", "Curious ", "Enticing "
-		};
-
-		level = rand() % NAMES_LENGTH;
-		i.name = qualityNames[quality] + names[level] + " potion";
-		quality += 1;
-		i.durValue = 1 + rand() % 2;
-
-		do i.keyValue = (quality - (rand() % (quality * 2))) * 2; while (i.keyValue == 0);
-
-		i.spriteCellX = 1;
-
+		return GetRandomHealing();
 		break;
 	default:
+		qualityNames = new std::string[QUALITY_LEVELS] { "Horrible ", "Worn ", "", "Nice ", "Great ", "Fantastic ", "Other worldly " };
+		names = new std::string[NAMES_LENGTH] { "stick", "sword", "rock", "lief", "thing" };
 		i.type = NO_USE_ITEM_TYPE;
+
+		i.name = names[level];
+
+		i.keyValue = 0;
+		i.durValue = 0;
+
+		i.spriteCellX = 3;
+		i.spriteCellY = 1 + (rand() % ITEM::MAX_CELL_Y);
 		break;
 	}
 
